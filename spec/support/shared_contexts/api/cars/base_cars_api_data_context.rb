40cde94cd8829967e9e9cdbaf06373c4eb970579
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2025-01-17 00:15:15 +0300

    Api::Cars::Interactor - moved general part to shared_context

---
+++ spec/support/shared_contexts/api/cars/base_cars_api_data_context.rb
@@ -0,0 +1,30 @@
+RSpec.shared_context 'with base cars API data' do
+  let(:user) do
+    record = create(:user, preferred_price_range: 25000...31000)
+    record.preferred_brands << brand
+
+    record
+  end
+  let(:attributes) { valid_attributes }
+  let(:valid_attributes) do
+    {
+      query: 'Volvo',
+      price_min: 25_000,
+      price_max: 50_000
+    }
+  end
+  let(:mocked_recommended_cars) do
+    [
+      {"car_id" => car.id, "rank_score" => 0.777}
+    ]
+  end
+  let(:brand) { create(:brand, name: 'Volvo') }
+  let!(:car) { create(:car, brand:, model: 'S70', price: 30_000) }
+
+  before do
+    api_instance = instance_double(
+      Api::Cars::RecommendedService, call: mocked_recommended_cars
+    )
+    allow(Api::Cars::RecommendedService).to receive(:new).and_return(api_instance)
+  end
+end
